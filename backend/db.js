646229d7de42fc99bda2672bd4a8a682bbe42750
Ajaba35 <ajaba35@gmail.com>
2025-05-12 09:44:34 +0100

    database settings change

--- backend/db.js
+++ backend/db.js
@@ -10,6 +10,8 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    connectTimeout: 30000,
+    port: process.env.DB_PORT
 });
 
 db.connect((err) => {
